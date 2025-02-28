65909d33df0aa1c852f69b46b5705b5c678b250c
Lucas Fryzek <lucas.fryzek@fryzekconcepts.com>
2025-02-28 21:33:39 +0000

    Add png favicon

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-git subtree push --prefix html origin gh-pages
+# Old deploy to gitlab pages
+# git subtree push --prefix html origin gh-pages
+
+rsync -avz --delete html/ game.fryzekconcepts.com:/var/www/blog/
